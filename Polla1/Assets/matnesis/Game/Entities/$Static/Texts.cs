1e7b52ffc9c7ca8fec9824a8e9eba637a753880d
Andrés Villalobos <andresalvivar@gmail.com>
2020-12-26 16:29:57 -0600

    The void rejects you

--- Polla1/Assets/matnesis/Game/Entities/$Static/Texts.cs
+++ Polla1/Assets/matnesis/Game/Entities/$Static/Texts.cs
@@ -3,21 +3,25 @@
     public static string INTRO = "\t\tYOU JUST DIED\n\t\tand HELL awaits\n\n\t\tYou have been cursed\n\t\twith THE EYE OF MINDS";
     public static string[] FADING_MEMORIES = new string[]
     {
-        "\t\tMy mother voice?",
-        "\t\tMy father?",
-        "\t\tWhere?",
-        "\t\tWhat's this?",
-        "\t\tI... never...",
-        "\t\t?",
-        "\t\tWhy?",
-        "\t\tGod?",
-        "\t\tAnyone?",
-        "\t\tHow is this possible?",
-        "\t\tWho remembers me?",
-        "\t\tWho loves me?",
-        "\t\tI wasn't enough",
-        "\t\tDoes it stink like rotten meat?",
-        "\t\t(WHAT HAPPENS TO A DREAM DEFERRED?)",
-        "\t\tWho said that?"
+        "\t\t\tMy mother voice?",
+        "\t\t\tMy father?",
+        "\t\t\tWhere?",
+        "\t\t\tWhat's this?",
+        "\t\t\tI... never...",
+        "\t\t\t?",
+        "\t\t\tWhy?",
+        "\t\t\tGod?",
+        "\t\t\tAnyone?",
+        "\t\t\tHow is this possible?",
+        "\t\t\tWho remembers me?",
+        "\t\t\tWho loves me?",
+        "\t\t\tI wasn't enough",
+        "\t\t\tDoes it stink like rotten meat?",
+        "\t\t\t(WHAT HAPPENS TO A DREAM DEFERRED?)",
+        "\t\t\tWho said that?",
+    };
+    public static string[] VOID_REJECTS_YOU = new string[]
+    {
+        "\t\tThe Void rejects you",
     };
 }
--- Polla1/Assets/matnesis/Game/Entities/VoidDetection/VoidDetectionSystem.cs
+++ Polla1/Assets/matnesis/Game/Entities/VoidDetection/VoidDetectionSystem.cs
@@ -2,12 +2,36 @@ using UnityEngine;
 
 public class VoidDetectionSystem : MonoBehaviour
 {
+    bool intoTheVoid = false;
+    MainMessage mainMessage;
+
     void Update()
     {
+        if (!mainMessage)
+            mainMessage = EntitySet.MainMessages.Elements[0];
+
         var voidDetections = EntitySet.VoidDetections;
         for (int i = 0; i < voidDetections.Length; i++)
         {
             var voidDetection = voidDetections.Elements[i];
+
+            if (intoTheVoid)
+                return;
+
+            if (voidDetection.transform.position.y <= -200)
+            {
+                intoTheVoid = true;
+                mainMessage.damp = 3;
+                mainMessage.mainText.text = "";
+                mainMessage.show = true;
+
+                this.tt("IntoTheVoid")
+                    .Reset()
+                    .Add(3, () =>
+                    {
+                        mainMessage.mainText.text = Texts.VOID_REJECTS_YOU[0];
+                    });
+            }
         }
     }
 }
