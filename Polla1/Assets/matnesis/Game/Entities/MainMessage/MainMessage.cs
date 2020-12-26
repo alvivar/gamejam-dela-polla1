f0a884ff2d5b263154531256d6cdb1ceff18cf5c
Andrés Villalobos <andresalvivar@gmail.com>
2020-12-26 15:09:39 -0600

    Game intro

--- Polla1/Assets/matnesis/Game/Entities/MainMessage/MainMessage.cs
+++ Polla1/Assets/matnesis/Game/Entities/MainMessage/MainMessage.cs
@@ -5,6 +5,9 @@ using UnityEngine.UI;
 // !Gigas
 public class MainMessage : MonoBehaviour
 {
+    public bool show = false;
+    public float damp = 3;
+
     [Header("Children References")]
     public Canvas canvas;
     public TextMeshProUGUI mainText;
---
+++ Polla1/Assets/matnesis/Game/Entities/Texts.cs
@@ -0,0 +1,23 @@
+﻿public class Texts
+{
+    public static string INTRO = "\t\tYOU JUST DIED\n\t\tand HELL awaits\n\n\t\tYou have been cursed\n\t\twith THE EYE OF MINDS";
+    public static string[] FADING_MEMORIES = new string[]
+    {
+        "\t\tMy mother voice?",
+        "\t\tMy father laugh?",
+        "\t\tWhere?",
+        "\t\tWhat's this?",
+        "\t\tI... never...",
+        "\t\tWhy?",
+        "\t\t?",
+        "\t\tGod?",
+        "\t\tDevil?",
+        "\t\tWho remembers me?",
+        "\t\tWho loves me?",
+        "\t\tHow is this possible?",
+        "\t\tDoes it stink like rotten meat?",
+        "\t\tI didn't make it",
+        "\t\t*What happens to a dream deferred?*",
+        "\t\tWho said that?"
+    };
+}
