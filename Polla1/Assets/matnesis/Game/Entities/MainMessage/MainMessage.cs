6e9fc1f50009d670d17f43485b58bd29addca6ae
Andrés Villalobos <andresalvivar@gmail.com>
2020-12-26 14:13:51 -0600

    Prefab ready

--- Polla1/Assets/matnesis/Game/Entities/MainMessage/MainMessage.cs
+++ Polla1/Assets/matnesis/Game/Entities/MainMessage/MainMessage.cs
@@ -1,8 +1,15 @@
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 // !Gigas
 public class MainMessage : MonoBehaviour
 {
+    [Header("Children References")]
+    public Canvas canvas;
+    public TextMeshProUGUI mainText;
+    public Image background;
+
     private void OnEnable()
     {
         EntitySet.AddMainMessage(this);
