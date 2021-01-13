230e0cda7a579b5a52e86138cc70367ed89b13f9
Andrés Villalobos <andresalvivar@gmail.com>
2021-01-13 02:41:44 -0600

    Pretty cool system hide things when you are not looking at it

--- Polla1/Assets/matnesis/Game/Entities/HideOnEye/HideOnEye.cs
+++ Polla1/Assets/matnesis/Game/Entities/HideOnEye/HideOnEye.cs
@@ -3,6 +3,11 @@ using UnityEngine;
 // !Gigas
 public class HideOnEye : MonoBehaviour
 {
+    public bool showing = true;
+    public bool flag;
+    public GameObject target;
+    public GameObject[] targets;
+
     private void OnEnable()
     {
         EntitySet.AddHideOnEye(this);
--- Polla1/Assets/matnesis/Game/Entities/HideOnEye/HideOnEyeSystem.cs
+++ Polla1/Assets/matnesis/Game/Entities/HideOnEye/HideOnEyeSystem.cs
@@ -2,13 +2,51 @@ using UnityEngine;
 
 public class HideOnEyeSystem : MonoBehaviour
 {
+    public float playerDistance;
+    public float dot;
+
+    Transform player;
+
     void Update()
     {
+        if (!player)
+            player = EntitySet.VoidCams.Elements[0].transform;
+
         var hideOnEyes = EntitySet.HideOnEyes;
         for (int i = 0; i < hideOnEyes.Length; i++)
         {
             var hideOnEye = hideOnEyes.Elements[i];
 
+            if (hideOnEye.targets.Length < 1)
+                continue;
+
+            var hidePos = hideOnEye.target.transform.position;
+            var playerPos = player.transform.position;
+
+            playerDistance = Vector3.Distance(hidePos, playerPos);
+
+            if (playerDistance < 25)
+                continue;
+
+            dot = Vector3.Dot(player.forward, (hidePos - playerPos).normalized);
+
+            if (dot < 0.20f)
+            {
+                if (!hideOnEye.flag)
+                {
+                    hideOnEye.flag = true;
+                    hideOnEye.showing = !hideOnEye.showing;
+
+                    for (int j = 0; j < hideOnEye.targets.Length; j++)
+                    {
+                        hideOnEye.targets[j].SetActive(hideOnEye.showing);
+                    }
+                }
+            }
+            else
+            {
+                hideOnEye.flag = false;
+            }
         }
     }
 }
