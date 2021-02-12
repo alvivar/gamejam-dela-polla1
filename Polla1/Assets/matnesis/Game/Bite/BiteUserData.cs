928a6717ee99cce870914fef3b77933cd78dc907
Andrés Villalobos <andresalvivar@gmail.com>
2021-02-11 23:35:41 -0600

    Ready to test on the polla

--- Polla1/Assets/matnesis/Game/Bite/BiteUserData.cs
+++ Polla1/Assets/matnesis/Game/Bite/BiteUserData.cs
@@ -4,20 +4,65 @@ public class BiteUserData : MonoBehaviour
 {
     private Bite bite;
 
+    private string app = "eyeOfMinds";
+    private string id = SystemInfo.deviceUniqueIdentifier;
+
+    private string username = "";
+    private float timePlayed = 0;
+
+    private float timer = 0;
+
     void Start()
     {
         bite = new Bite("142.93.180.20", 1984);
 
         bite.OnError = OnError;
         bite.OnResponse = OnResponse;
+
+        Load();
     }
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        // Save every second.
+
+        if (Time.time < timer)
+            return;
+        timer = Time.time + 1;
+
+        timePlayed += 1;
+
+        Save();
+    }
+
+    void Load()
+    {
+        bite.Send($"g {app}.{id}.name", response =>
         {
-            bite.Send("j");
-        }
+            if (response.Trim().Length < 1)
+                response = "?";
+
+            username = response;
+        });
+
+        bite.Send($"g {app}.{id}.timePlayed", response =>
+        {
+            var n = 0;
+            var y = int.TryParse(response, out n);
+
+            timePlayed = y ? n : 0;
+        });
+    }
+
+    void Save()
+    {
+        bite.Send($"s {app}.{id}.timePlayed {timePlayed}");
+    }
+
+    void SetName(string name)
+    {
+        username = name;
+        bite.Send($"s {app}.{id}.name {username}");
     }
 
     void OnResponse(string response)
