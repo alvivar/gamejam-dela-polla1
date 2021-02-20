251a2e93fdfb8cc4654790506097ffb104e88843
Andrés Villalobos <andresalvivar@gmail.com>
2021-02-20 15:48:16 -0600

    Better

--- Polla1/Assets/matnesis/Game/Entities/Analytics/OnFirstLastPosition.cs
+++ Polla1/Assets/matnesis/Game/Entities/Analytics/OnFirstLastPosition.cs
@@ -20,21 +20,26 @@ public class OnFirstLastPosition : MonoBehaviour
             message = EntitySet.MainMessages.Elements[0];
 
         this.tt()
-            .Wait(() => analytics.data.lastPosition != Vector3.zero, 1)
+            .Wait(() => analytics.data.lastPosition != Vector3.zero, 0.1f)
             .Add(() =>
             {
+                lastPosition = analytics.data.lastPosition;
+
                 message.mainDamp = 10;
                 message.main.text = "";
                 message.showMain = true;
+
                 player.fps.enabled = false;
             })
             .Add(0.7f, () =>
             {
-                player.transform.position = lastPosition = analytics.data.lastPosition;
+                player.transform.position = lastPosition;
             })
             .Add(0.3f, () =>
             {
+                message.mainDamp = 1;
                 message.showMain = false;
+
                 player.fps.enabled = true;
             });
     }
